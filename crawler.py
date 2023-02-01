46fd9e040fe56b3ad4181051cdde36117ed8c03a
antaechan <0627antaechan@gmail.com>
2023-02-01 23:49:26 +0900

    Add: save taxi articles in DB

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pymongo import MongoClient
 
 load_dotenv()
 
@@ -24,25 +25,42 @@ if __name__ == "__main__":
     login_button = browser.find_element(By.XPATH, "//*[@id='container']/form/p[3]/input")
     login_button.click()
     
+    # localhost의 mongoDB 연결
+    client = MongoClient(host='localhost', port=27017)
+    print(client.list_database_names())
+
+    # DB 접근
+    local = client['local']
+    print(local.list_collection_names())
+
+    if "everytime_taxi_articles" in local.list_collection_names():
+        pass
+    else:
+        # taxi_articles collection 생성
+        everytime_taxi_articles = local.create_collection('everytime_taxi_articles')
+        print(local.list_collection_names())
+    
+    everytime_taxi_articles = local["everytime_taxi_articles"]
+    
     # 택시 게시판 html 요청
     for page_number in range(page_number_end + 1):
         taxi_url = taxi_url_prefix + str(page_number)
         browser.get(taxi_url)
         taxi_page =  browser.page_source
-        browser.quit()
         
         soup = BeautifulSoup(taxi_page, "html5lib")
         
         wrap_articles = soup.find("div", attrs = {"class":"wrap articles"})
-        articles = wrap_articles.find_all("article")
-                
-        for article in articles:
-            new_articles = [article.text for article in articles]
-            
-        print(new_articles)
+        raw_articles = wrap_articles.find_all("article")
         
-        while True:
-            continue
+        for raw_article in raw_articles:
+            upload_time = raw_article.find("time").text
+            context = raw_article.find("p", attrs = {"class":"medium"}).text
+            
+            article = { "upload_time": upload_time, "context": context }
+            everytime_taxi_articles.insert_one(article)
+        
+        
 
 
 
